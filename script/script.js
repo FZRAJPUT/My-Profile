3d4a892174d417a42feb0463a25ddf0fd67a3e32
Subhash Kumar <142812196+FZRAJPUT@users.noreply.github.com>
2024-07-17 12:30:51 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,20 +1,21 @@
 const menu = document.getElementById('opt');
 const menuIcon = document.getElementById('icon');
 
-
 var flag = true;
 menuIcon.addEventListener('click',()=>{
 
     if(flag){
-        menu.style.display = "flex" ;
+        menu.style.display = "flex";
+        menuIcon.classList.remove('ri-menu-line');
+        menuIcon.classList.add('ri-close-line');
     }
     else{
-        menu.style.display = "none" ;
+        menu.style.display = "none";
+        menuIcon.classList.remove('ri-close-line');
+        menuIcon.classList.add('ri-menu-line');
     }
 
     flag = !flag;
 })
-
-
 
 
